ad8f30f9e175382f91aacb96ff8b7e96b08ea994
AbhiramNe <s-nerellaa@bsd405.org>
2025-05-23 11:00:24 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,1 +1,17 @@
+public class Card {
+    private String symbol;
+    private int num;
 
+    public Card(String symbol, int num){
+        this.symbol = symbol;
+        this.num = num;
+    }
+    
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getNum() {
+        return num;
+    }
+}
