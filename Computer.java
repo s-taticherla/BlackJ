9626a959dcef135a51691af5bfca3379e0670e86
AbhiramNe <s-nerellaa@bsd405.org>
2025-05-30 10:45:04 -0700

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -1,1 +1,22 @@
+import java.util.ArrayList;
+
+public class Computer {
+    private ArrayList<Card> computer_hand = new ArrayList<>();
+
+    public void add(Card e) {
+        computer_hand.add(e);
+    }
+
+    public Card access(int i) {
+        return computer_hand.get(i);
+    }
+
+    public int sum() {
+        int sum = 0;
+        for(int i = 0; i < computer_hand.size(); i++) {
+            sum += computer_hand.get(i).getNum();
+        }
+        return sum;
+    }
+}
 
