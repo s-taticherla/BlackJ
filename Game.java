b18185f0727658b9d2e1cc1c83aa994fc9438fc6
AbhiramNe <s-nerellaa@bsd405.org>
2025-06-03 10:45:09 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,8 +1,21 @@
 public class Game {
+    private static boolean player_won = false;
+    private static boolean computer_won = false;
+
     public static void main(String[] args) {
-    Deck deck = new Deck();
-    Card ranCard = deck.randCard();
-    Player p1 = new Player();
-    p1.addCard(ranCard);
+        Deck deck = new Deck();
+        Card ranCard = deck.randCard();
+        Player p1 = new Player();
+        Player computer = new Player();
+        p1.addCard(ranCard);
+        ranCard = deck.randCard();
+        p1.addCard(ranCard);
+        ranCard = deck.randCard();
+        computer.addCard(ranCard);
+        ranCard = deck.randCard();
+        computer.addCard(ranCard);
+        while (player_won == false && computer_won == false) {
+            
+        }
     }
 }
