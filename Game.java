59b623fba9a6e0291264d368b7ddcdc7c16c48f3
AbhiramNe <s-nerellaa@bsd405.org>
2025-06-03 10:12:36 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,6 +1,8 @@
 public class Game {
+    public static void main(String[] args) {
     Deck deck = new Deck();
     Card ranCard = deck.randCard();
-    Player p = new Player();
-    p.addCard(ranCard);
+    Player p1 = new Player();
+    p1.addCard(ranCard);
+    }
 }
