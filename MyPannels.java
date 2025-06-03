5ca447a2432a0dad813adb4483bc3c61ae8faca6
s-taticherla <sameeertaticherla@gmail.com>
2025-06-03 10:44:42 -0700

    Update MyPannels.java

--- MyPannels.java
+++ MyPannels.java
@@ -4,17 +4,43 @@ import java.awt.event.*;
 
 public class MyPannels extends JPanel{
 	Graph g;
-	private Image im;
-	public MyPannels(Graph gr, Image I) {
+	ML a;
+	Player play;
+	Player comp;
+	
+	public MyPannels(Graph gr) {
 		this.g = gr;
-		this.im = I;
-		addMouseListener(new ML(this, this.g));
+		this.a = new ML(this, this.g);
+		addMouseListener(this.a);
 	}
 	
 	public void paintComponent(Graphics g) {
 		// to remove all face up drawings do : super.paintComponent(g);
-		g.drawImage(this.im, 50, 50, 200, 150, this);
-		this.g.test(g, this.g.getX(), this.g.getY());
+		this.g.drawCard(g, this.g.getX(), this.g.getY(), "♥", compute(12), compute("♥"));
+	}
+	
+	private String compute(int num) {
+		if (num <= 10 && num !=1) {
+			return ""+num;
+		}
+		else {
+			if(num == 1) {
+				return "A";
+			}
+			else if (num == 11) {
+				return "J";
+			}
+			else if (num == 12) {
+				return "Q";
+			}
+			else {
+				return "K";
+			}
+		}
+	}
+	
+	private boolean compute(String suit) {
+		return (suit.equals("♥")||suit.equals("♦️"));
 	}
 }
 class ML extends MouseAdapter {
