085fe935ebcbb7a27b6319fedc34c30febdf2bb2
AbhiramNe <s-nerellaa@bsd405.org>
2025-06-05 11:18:33 -0700

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -9,7 +9,7 @@ public class Deck{
         int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
         for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 13; i++) {
+            for(int j = 0; j < 13; j++) {
                 deck.add(new Card(symbols[i], number[j]));
             }
         }
