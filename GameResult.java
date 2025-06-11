10d8db3b822dc52fb06c0e47ba51653477cf626a
AbhiramNe <s-nerellaa@bsd405.org>
2025-06-10 22:29:14 -0700

    Update GameResult.java

--- GameResult.java
+++ GameResult.java
@@ -1,1 +1,17 @@
+public class GameResult {
+    private boolean comp_won;
 
+    public GameResult(int compSum, int playerSum) {
+        if(compSum > playerSum) {
+            comp_won = true;
+        }else if(playerSum > compSum) {
+            comp_won = false;
+        }else if(playerSum == compSum) {
+            comp_won = true;
+        }
+    }
+
+    public boolean getResult() {
+        return comp_won;
+    }
+}
