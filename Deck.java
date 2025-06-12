f956ea95312850348be681250600295fe615dbaa
s-taticherla <sameeertaticherla@gmail.com>
2025-06-12 11:04:25 -0700

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -8,8 +8,8 @@ public class Deck{
         String[] symbols =  {"♥", "♦️", "♠", "♣"};
         int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < 13; j++) {
+        for(int i = 0; i < symbols.length; i++) {
+            for(int j = 0; j < number.length; j++) {
                 deck.add(new Card(symbols[i], number[j]));
             }
         }
